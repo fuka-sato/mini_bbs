640d0c80bbf77a041578a0a6e7a0fd5f964bf068
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-09-26 23:26:02 +0900

    投稿日の表示

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ $posts = $db->query('SELECT m.name,m.picture,p.* FROM members m ,posts p WHERE m
     <div class="msg">
     <img src="member_picture" width="48" height="48" alt="" />
     <p><?php print(htmlspecialchars($post['message'],ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
+    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'],ENT_QUOTES)); ?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
